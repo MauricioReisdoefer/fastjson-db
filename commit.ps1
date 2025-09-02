82d09241c6e0f743bb8958e7ceda48cbb4e6318b
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-09-02 13:12:43 -0300

    Improving event more ./commit.ps1

--- commit.ps1
+++ commit.ps1
@@ -16,20 +16,30 @@ if ($LASTEXITCODE -eq 0 -or $LASTEXITCODE -eq 5) {
 
     # Pedir mensagem do commit
     Write-Host ""
-    $commitMessage = Read-Host "Digite a mensagem do commit: "
+    $commitMessage = Read-Host "Digite a mensagem do commit"
 
     # Fazer commit
     git add .
     git commit -m "$commitMessage"
     git push origin main
 
+    $width = 34
+    function CenterText($text, $width) {
+        $padding = [Math]::Max(0, ($width - $text.Length) / 2)
+        return (" " * [Math]::Floor($padding)) + $text
+    }
+
     Clear-Host
-    Write-Host "==================================" -ForegroundColor DarkCyan
-    Write-Host " 🚀 Commit enviado com sucesso! " -ForegroundColor Cyan
-    Write-Host "==================================" -ForegroundColor DarkCyan
-}
+    Write-Host ("=" * $width) -ForegroundColor DarkCyan
+    Write-Host (CenterText "Commit enviado com sucesso!" $width) -ForegroundColor Cyan
+    Write-Host ""
+    Write-Host (CenterText "Mensagem" $width) -ForegroundColor Yellow
+    Write-Host (CenterText $commitMessage $width) -ForegroundColor White
+    Write-Host ""
+    Write-Host ("=" * $width) -ForegroundColor DarkCyan
+    }
 else {
     Write-Host "==================================" -ForegroundColor DarkRed
-    Write-Host " ❌ Testes falharam. Commit cancelado. " -ForegroundColor Red
+    Write-Host "  Testes falharam. Commit cancelado. " -ForegroundColor Red
     Write-Host "==================================" -ForegroundColor DarkRed
 }
